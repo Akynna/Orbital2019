32c21b5562d0af38a7d3e2af0897cd9055a3eff8
Serwyn <sim-guilloud@bluewin.ch>
2019-03-02 23:29:10 +0100

    wsrcdtj
    ZRQexstdrcr

---
+++ NotreJeu/Assets/Scripts/Questions.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Questions : MonoBehaviour
+{
+    public Text questionText;
+    public Text answerText;
+    public GameObject Vampire;
+    int n;
+    int m;
+    int r;
+    int pending = 0;
+    static System.Random rnd = new System.Random();
+    bool isRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateNewQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pending += 1;
+        if (pending == 60)
+        {
+            isRight= rnd.Next(0,2)!=0;
+            int error = Random.Range(1,3)*new List<int>() {-100,-10,-1, 1, 10, 100 }[rnd.Next(0,3)];
+            if (isRight){
+                answerText.text = "" + r;
+            }
+            else
+            {
+                answerText.text = "" + (r+error);
+            }
+            
+        }
+        
+    }
+
+    public void CreateNewQuestion()
+    {
+        pending = 0;
+        n = Random.Range(0, 999);
+        m = Random.Range(0, 999);
+        r = n + m;
+        questionText.text = n + " + " + m + " = ?";
+        answerText.text = "";
+
+    }
+    public void Valider()
+    {
+        CreateNewQuestion();
+    }
+
+    public void TuAsTort()
+    {
+        if (pending > 60)
+        {
+            Vampire.GetComponent<VampireMain>().TuAsTort(isRight);
+     
+        }
+    }
+}
---
+++ NotreJeu/Assets/Scripts/VampireMain.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VampireMain : MonoBehaviour
+{
+    public int life = 3;
+    public int score = 0;
+    public int TargetScore = 5;
+    public string firstTextBox = "Je me suis assez empifré de ces incapables...";
+    public string secondTextBox = "Il est temps de passer à un plat un peu plus coriace !";
+
+    public Sprite Idle;
+    public Sprite RaiseHand;
+    public GameObject Director;
+    public GameObject BlackBoard;
+    public GameObject TuAsTortSprite;
+    public List<GameObject> Coeurs;
+    public GameObject Bulle;
+    public GameObject BulleD;
+    public GameObject fadeOut;
+    SpriteRenderer sprite;
+    int Timer = 181;
+    bool wrong;
+    bool changerEcole = false;
+    bool losing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        sprite = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += 1;
+        if (Timer == 120 && ! changerEcole && !losing)
+        {
+            if (wrong)
+            {
+                Lose();
+            }
+            else
+            {
+                MangerEleve();
+            }
+            sprite.sprite = Idle;
+            TuAsTortSprite.SetActive(false);
+
+        }
+        if (Timer == 121 && !changerEcole)
+        {
+
+            BlackBoard.GetComponent<Questions>().CreateNewQuestion();
+        }
+            if (Timer == 120 && changerEcole)
+        {
+            Bulle.GetComponentInChildren<Text>().text = secondTextBox;
+
+        }
+        if (Timer >= 240 && Timer <= 480 && changerEcole)
+        {
+            fadeOut.GetComponent<SpriteRenderer>().color = new Color(0,0,0, ((float)(Timer-240))/240);
+            fadeOut.SetActive(true);
+        }
+        if (Timer == 480 && changerEcole)
+        {
+            Bulle.SetActive(false);
+
+        }
+        if (Timer == 540 && changerEcole)
+        {
+            fadeOut.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+            fadeOut.SetActive(false);
+            changerEcole = false;
+        }
+        if (Timer == 120 && losing)
+        {
+            BulleD.SetActive(false);
+            Director.SetActive(false);
+            losing = false;
+
+        }
+    }
+    public void TuAsTort(bool w)
+    {
+        wrong = w;
+        TuAsTortSprite.SetActive(true);
+        sprite.sprite = RaiseHand;
+        Timer = 0;
+    }
+    public void Lose()
+    {
+
+        life -= 1;
+        Coeurs[life].SetActive(false);
+        losing = true;
+        BulleD.SetActive(true);
+        Director.SetActive(true);
+        Timer = 0;
+    }
+
+    public void MangerEleve()
+    {
+        score += 1;
+        if (score == TargetScore)
+        {
+            changerEcole=true;
+            Bulle.SetActive(true);
+            Bulle.GetComponentInChildren<Text>().text = firstTextBox;
+            Timer = 0;
+        }
+    }
+}
